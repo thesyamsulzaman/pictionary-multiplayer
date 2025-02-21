1a310f431d31f3cb0a23e614e98514319a537cac
Syamsul Zaman <thesyamsulzaman@gmail.com>
2025-02-21 14:37:05 +0700

    clear

--- server/utilities.js
+++ server/utilities.js
@@ -28,12 +28,4 @@ const getRandomColor = () => {
 }
 
 
-const getClientId = () => {
-  function S4() {
-    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-  }
-
-  return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0, 3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase()
-}
-
-module.exports = { getRandomPet, getRandomColor, getClientId }
+module.exports = { getRandomPet, getRandomColor }
